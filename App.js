269b2472cc5b3f5cff8f366998f39a9b915d3f8f
jw0918.kim <jw0918.kim@samsung.com>
2018-12-20 13:37:45 +0900

    Updating a To Do

--- App.js
+++ App.js
@@ -52,6 +52,7 @@ export default class App extends React.Component {
                 deleteToDo={this._deleteToDo}
                 uncompleteToDo={this._uncompleteToDo}
                 completeToDo={this._completeToDo}
+                updateToDo={this._updateToDo}
                 {...toDo}
               />
             ))}
@@ -128,6 +129,18 @@ export default class App extends React.Component {
         toDos: {
           ...prevState.toDos,
           [id]: { ...prevState.toDos[id], isCompleted: true }
+        }
+      };
+      return { ...newState };
+    });
+  };
+  _updateToDo = (id, text) => {
+    this.setState(prevState => {
+      const newState = {
+        ...prevState,
+        toDos: {
+          ...prevState.toDos,
+          [id]: { ...prevState.toDos[id], text: text }
         }
       };
       return { ...newState };
